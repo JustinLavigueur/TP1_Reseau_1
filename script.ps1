0fc637dbfd9d929654f326704d8178f82a7da5ee
JustinLavigueur <Lavijustin@hotmail.com>
2025-09-19 17:03:53 -0400

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,7 @@
+# Voici une conversion  de mon fichier guide_etudiants_sciences.md (Markdown) en DokuWiki
+
+# Ligne qui va me permettre de converser le fichier .md en document dokuwiki
+
+pandoc -f markdown -t dokuwiki "guide_etudiants_sciences.md" -o "documentation.dokuwiki"
+
+Write-Output "Conversion du fichier "guide_etudiants_sciences.md" en dokuwiki finie !"
